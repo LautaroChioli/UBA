b2abdaa43618341b386c4caf00a318faf1017a60
LautaroChioli <80519739+LautaroChioli@users.noreply.github.com>
2025-04-04 15:16:38 -0300

    Hasta Ej2 f

---
+++ Guia 3/ejercicio_dos.hs
@@ -0,0 +1,41 @@
+absoluto :: Int -> Int
+absoluto n 
+    | n < 0 = -n
+    | otherwise = n
+
+maximoDeAbsolutos :: Int -> Int -> String
+maximoDeAbsolutos n m
+    | absoluto n > absoluto m = "n mayor que m"
+    | absoluto n < absoluto m = "m mayor que n"
+
+
+maximoDeTres :: Int -> Int -> Int -> String
+maximoDeTres x y z
+    | x > y && x > z = "El primer numero es el mayor"
+    | y > z && y > x = "El segundo numero es el mayor"
+    | otherwise = "El tercer numero es el mayor"
+
+algunoEsCeroGuards :: Int -> Int -> String
+algunoEsCeroGuards n m 
+    | n == 0 || m == 0 = "Uno de los dos numeros es cero"
+    | otherwise = "Ninguno es cero"
+
+algunoEsCeroPattern :: Int -> Int -> String
+algunoEsCeroPattern 0 m = "Uno es cero"
+algunoEsCeroPattern n 0 = "Uno es cero"
+algunoEsCeroPattern n m = "Ninguno es cero"
+
+ambosSonCeroGuards :: Int -> Int -> String
+ambosSonCeroGuards n m
+    | n == 0 && m == 0 = "Ambos son cero"
+    | otherwise = "Uno de los dos no es cero"
+
+ambosSonCeroPattern :: Int -> Int -> String
+ambosSonCeroPattern 0 0 = "Ambos son cero"
+ambosSonCeroPattern n m = "Uno de los dos no es cero! Putoooooo!!!!!"
+
+enMismoIntervalo :: Float -> String
+enMismoIntervalo n
+    | n <= 3 = "Entre menos infinito y tres, incluido."
+    | n <= 7 = "Entre tres y siete, incluido."
+    | otherwise = "Entre siete y mas infinito"
