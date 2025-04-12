2b718d4f52a7a867473d77e0ff02f6e0861de666
LautaroChioli <80519739+LautaroChioli@users.noreply.github.com>
2025-04-12 18:14:05 -0300

    Jijijiji

--- haskell/Guia 4/ejerciciodos.hs
+++
@@ -1,9 +0,0 @@
-contarDesde0 :: Float -> Int -> Int
-contarDesde0 n inicio
-    | fromIntegral inicio <= n && n < fromIntegral (inicio + 1) = inicio
-    | otherwise = contarDesde0 n (inicio + 1)
-
-parteEntera :: Float -> Int
-parteEntera n
-  | n < 1     = 0
-  | otherwise = contarDesde0 n 0
