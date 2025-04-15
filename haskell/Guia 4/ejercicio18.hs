775f07aa0bb7737f0ac43a1d8da5b30dec3ad342
LautaroChioli <80519739+LautaroChioli@users.noreply.github.com>
2025-04-15 19:38:28 -0300

    guia 4 Jijo

---
+++ haskell/Guia 4/ejercicio18.hs
@@ -0,0 +1,14 @@
+cantDigitos :: Int -> Int
+cantDigitos n
+  | n < 10    = 1
+  | otherwise = 1 + cantDigitos (div n 10)
+
+iesimoDigito :: Int -> Int -> Int
+iesimoDigito n digito = mod (div n (10 ^ (cantDigitos n - digito))) 10
+
+
+mayorPar :: Int -> Int -> Int
+mayorPar n maximo
+  | n == 0 = maximo
+  | mod n 2 == 0 && maximo < mod n 10 = mayorPar (div n 10) (mod n 10)
+  | otherwise = mayorPar (div n 10) maximo
--- haskell/Guia 4/ejercicio9.hs
+++ haskell/Guia 4/ejercicio9.hs
@@ -1,1 +1,14 @@
-??????????????????????????
+cantDigitos :: Int -> Int
+cantDigitos n
+    | n < 10 = 1
+    |  n >= 10 = 1 + cantDigitos (div n 10)
+
+iesimoDigito :: Int -> Int -> Int
+iesimoDigito n digito = mod (div n (10 ^ (cantDigitos n - digito))) 10
+
+
+esCapicua :: Int -> Bool
+esCapicua n
+    | n < 10 = True
+    | iesimoDigito n (cantDigitos n) == iesimoDigito n ((cantDigitos n) - (cantDigitos n) + 1) = esCapicua (div (mod n (10 ^ (cantDigitos n - 1))) 10)
+    | otherwise = False
