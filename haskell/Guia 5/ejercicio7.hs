4fc1411f3d9432237fc7bf6315095ae6c4e3471d
LautaroChioli <80519739+LautaroChioli@users.noreply.github.com>
2025-04-30 13:41:39 -0300

    Add files via upload

---
+++ haskell/Guia 5/ejercicio7.hs
@@ -0,0 +1,55 @@
+type Identificacion = Integer
+type Ubicacion = String
+type Estado = (Disponibilidad, Ubicacion)
+type Locker = (Identificacion, Estado)
+type MapaDeLockers = [Locker]
+type Disponibilidad = Bool
+
+--lockers = [(100,(False,"ZD39I")),(101,(True,"JAH3I")),(103,(True,"IQSA9")),(105,(True,"QOTSA")),(109,(False,"893JJ")),(110,(False,"99292"))]
+
+-- Ejercicio I
+
+existeElLocker :: Identificacion -> MapaDeLockers -> Bool
+existeElLocker _ [] = False
+existeElLocker iden (locker:lockers)
+    | idenDeLocker locker == iden = True
+    | otherwise = existeElLocker iden lockers
+
+idenDeLocker :: Locker -> Identificacion
+idenDeLocker (iden, _) = iden
+
+-- Ejercicio II
+
+ubicacionDelLocker :: Identificacion -> MapaDeLockers -> Ubicacion
+ubicacionDelLocker iden (locker:lockers)
+    | existeElLocker iden (locker:lockers) && idenDeLocker locker == iden = ubiLocker (estadoDelLocker locker)
+    | otherwise = ubicacionDelLocker iden lockers
+
+estadoDelLocker :: Locker -> Estado
+estadoDelLocker (_, estado ) = estado
+
+ubiLocker :: Estado -> Ubicacion
+ubiLocker (_, ubicacion) = ubicacion
+
+-- Ejercicio III
+
+estaDisponibleElLocker :: Identificacion -> MapaDeLockers -> Bool
+estaDisponibleElLocker iden (locker:lockers)
+    |existeElLocker iden (locker:lockers) && idenDeLocker locker == iden = disponibilidadLocker (estadoDelLocker locker)
+    | otherwise = estaDisponibleElLocker iden lockers
+
+disponibilidadLocker :: Estado -> Disponibilidad
+disponibilidadLocker (disp, _) = disp
+
+-- Ejercicio IV
+
+-- ocuparLocker :: Identificacion -> MapaDeLockers -> MapaDeLockers
+
+
+sacarLocker :: Identificacion -> MapaDeLockers -> MapaDeLockers
+sacarLocker iden (locker:lockers)
+    | existeElLocker iden (locker:lockers) && idenDeLocker locker == iden = lockers
+    | otherwise = locker : sacarLocker iden lockers
+
+-- ocuparLocker :: Identificacion -> MapaDeLockers -> Locker 
+-- ocuparLocker 
