485e8ba909be601c76be2473d91c9a272361e322
Lautaro Chioli <80519739+LautaroChioli@users.noreply.github.com>
2025-08-22 13:16:48 -0300

    Taller 1

--- Algo-2025-2do Cuatri/Talleres/taller1/src/main/java/aed/Funciones.java
+++ Algo-2025-2do Cuatri/Talleres/taller1/src/main/java/aed/Funciones.java
@@ -5,69 +5,141 @@ class Funciones {
 /***  Primera parte: Funciones en java ***/
 
     int cuadrado(int x) {
-        // COMPLETAR
-        return 0;
+        return x * x;
     }
 
     double distancia(double x, double y) {
-        // COMPLETAR
-        return 0.0;
+        return Math.sqrt(x * x + y * y);
     }
 
     boolean esPar(int n) {
-        // COMPLETAR
-        return false;
+        if (n%2 == 0) {
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 
     boolean esBisiesto(int n) {
-        // COMPLETAR
-        return false;
+        if (n%400 == 0) {
+            return true;
+        }
+        else if (n%100 == 0) {
+            return false;
+        }
+        else if (n%4 == 0){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 
     int factorialIterativo(int n) {
-        // COMPLETAR
-        return 0;
+        int res = 1;
+        if (n == 0){
+            return 1;
+        }
+        for (int m = n; m > 0; m--){
+            res *= m;
+        }
+        return res;
     }
 
     int factorialRecursivo(int n) {
-        // COMPLETAR
-        return 0;
-    }
+        int res = n;
+        if (n == 0 || n == 1){
+            return 1;
+        }
+        res *= factorialRecursivo(n - 1);
+        return res;
+        
+        }
+
+        
+    
+
 
     boolean esPrimo(int n) {
-        // COMPLETAR
+        int res = 0;
+        for (int divisor = n - 1; divisor > 0; divisor--){
+            if (n%divisor == 0){
+                res += 1;
+            }
+            
+            }
+        if (res == 1){
+            return true;
+        }
         return false;
-    }
+        }
+        
+    
 
     int sumatoria(int[] numeros) {
-        // COMPLETAR
-        return 0;
+        int res = 0;
+        for (int i = 0; i < numeros.length; i++){
+            res += numeros[i];
+
+        }
+        return res;
     }
 
     int busqueda(int[] numeros, int buscado) {
-        // COMPLETAR
-        return 0;
+        int res = 0;
+        for (int i = 0; i < numeros.length; i++){
+            if (numeros[i] == buscado){
+                res = i;
+            }
+        }
+        return res;
     }
 
     boolean tienePrimo(int[] numeros) {
-        // COMPLETAR
+        for (int i = 0; i < numeros.length;i++){
+            if (esPrimo(numeros[i])){
+                return true;
+            }
+        }
         return false;
     }
 
     boolean todosPares(int[] numeros) {
-        // COMPLETAR
-        return false;
+         for (int i = 0; i < numeros.length;i++){
+            if (! esPar(numeros[i])){
+                return false;
+            }
+        }
+        return true;
     }
 
     boolean esPrefijo(String s1, String s2) {
-        // COMPLETAR
-        return false;
+        if (s1.length() > s2.length()){
+                return false;
+            }
+        for (int i = 0; i < s1.length(); i++){
+            
+            if (s1.charAt(i) != s2.charAt(i)){
+                return false;
+            }
+        }
+        return true;
     }
 
     boolean esSufijo(String s1, String s2) {
-        // COMPLETAR
-        return false;
+       if (s1.length() > s2.length()){
+                return false;
+            }
+        for (int i = s1.length(); i < 0; i--){
+            
+            if (s1.charAt(i) != s2.charAt(i)){
+                return false;
+            }
+        }
+        return true;
     }
+    
 
 /***  Segunda parte: Debugging ***/
 
